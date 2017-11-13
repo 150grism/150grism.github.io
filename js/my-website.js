5b992e9507fc08794e1724e96c8a547b5a06d39c
Grigory Moshkevich <ilcostuff@gmail.com>
2017-11-13 17:00:21 +0300

    responsive nav menu - working on it

--- js/my-website.js
+++ js/my-website.js
@@ -1,14 +1,16 @@
-var menuButton = document.querySelector('.mobile-menu');
+var menuButton = document.querySelectorAll('.mobile-menu');
 var navOverlay = document.querySelector('.nav-overlay');
 var overlayVisible = false;
 
-menuButton.onclick = function() {
-  if (overlayVisible === false) {
-    navOverlay.style.width = "100%";
-    overlayVisible = true;
-  } else {
-    navOverlay.style.width = "0%";
-    overlayVisible = false;
+menuButton.forEach((oneButton) => {
+  oneButton.onclick = function() {
+    if (overlayVisible === false) {
+      navOverlay.style.width = "100%";
+      overlayVisible = true;
+    } else {
+      navOverlay.style.width = "0%";
+      overlayVisible = false;
+    }
+    // console.log('sadf');
   }
-  // console.log('sadf');
-}
+})
