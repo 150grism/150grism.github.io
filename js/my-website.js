271d19a0228c8b4a601c951def13fc0f97895f84
Grigory Moshkevich <ilcostuff@gmail.com>
2017-11-14 10:48:06 +0300

    ссылки заработали

--- js/my-website.js
+++ js/my-website.js
@@ -1,11 +1,13 @@
-var menuButton = document.querySelectorAll('.mobile-menu');
+var menuButtons = document.querySelectorAll('.mobile-menu');
 var navOverlay = document.querySelector('.nav-overlay');
-var navContainer = document.querySelector('.nav-container');
 var navWide = document.querySelector('.nav-wide');
+var goToHireButton = document.querySelector('.go-to-hire');
+var contactContainer = document.querySelector('.contact-container');
+var navLinks = document.querySelectorAll('.nav-link');
 var overlayVisible = false;
 
-menuButton.forEach((oneButton) => {
-  oneButton.onclick = () => {
+menuButtons.forEach((b) => {
+  b.onclick = () => {
     if (overlayVisible === false) {
       navOverlay.style.width = "100%";
       overlayVisible = true;
@@ -13,7 +15,25 @@ menuButton.forEach((oneButton) => {
       navOverlay.style.width = "0%";
       overlayVisible = false;
     }
-    // console.log('sadf');
+  }
+});
+
+// goToHireButton.onclick = () => {
+//   // console.log(contactContainer.offsetTop);
+//   window.scrollBy({top: contactContainer.offsetTop - 62 - 50, left: 0, behavior: "smooth"});
+// };
+
+navLinks.forEach((b) => {
+  b.onclick = () => {
+    // console.log(b.dataset.destination);
+    let destinationElement = document.getElementById(b.dataset.destination);
+    let distanceToDestination = destinationElement.offsetTop - window.pageYOffset - 62;
+    console.log(destinationElement.offsetTop);
+    window.scrollBy({top: distanceToDestination - 50, left: 0, behavior: "smooth"});
+    if (overlayVisible) {
+      navOverlay.style.width = "0%";
+      overlayVisible = false;
+    }
   }
 });
 
@@ -27,4 +47,4 @@ window.onscroll = () => {
     navWide.classList.remove('nav-scroll');
     navOverlay.firstElementChild.style.margin = "31px 30px 25px 30px";
   }
-}
+};
