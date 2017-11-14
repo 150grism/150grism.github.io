f739cf06bcc969248ecdd7c78294bd704c676cbb
Grigory Moshkevich <ilcostuff@gmail.com>
2017-11-14 08:41:45 +0300

    пфффф

--- js/my-website.js
+++ js/my-website.js
@@ -1,6 +1,7 @@
 var menuButton = document.querySelectorAll('.mobile-menu');
 var navOverlay = document.querySelector('.nav-overlay');
 var navContainer = document.querySelector('.nav-container');
+var navWide = document.querySelector('.nav-wide');
 var overlayVisible = false;
 
 menuButton.forEach((oneButton) => {
@@ -20,10 +21,10 @@ console.log(window.scrollY);
 
 window.onscroll = () => {
   if (window.scrollY > 0) {
-    navContainer.classList.add('nav-scroll');
+    navWide.classList.add('nav-scroll');
     navOverlay.firstElementChild.style.margin = "20.5px 29px 25px 31px";
   } else {
-    navContainer.classList.remove('nav-scroll');
+    navWide.classList.remove('nav-scroll');
     navOverlay.firstElementChild.style.margin = "31px 30px 25px 30px";
   }
 }
