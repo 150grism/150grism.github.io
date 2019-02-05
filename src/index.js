4d2035647dbb9f7260defb77e1eff6bffa0c0c88
Grigory Moshkevich <rehfres@gmail.com>
2019-02-05 17:45:43 +0300

    trying to add svg animation

--- src/index.js
+++ src/index.js
@@ -1,44 +1,28 @@
 import './css/style.scss'
-var menuButtons = document.querySelectorAll('.mobile-menu');
-var navOverlay = document.querySelector('.nav-overlay');
-var navWide = document.querySelector('.nav-wide');
-var navLinks = document.querySelectorAll('.nav-link');
-var overlayVisible = false;
 
-menuButtons.forEach((b) => {
-  b.onclick = () => {
-    if (overlayVisible === false) {
-      navOverlay.style.width = "100%";
-      overlayVisible = true;
-    } else {
-      navOverlay.style.width = "0%";
-      overlayVisible = false;
-    }
-  }
-});
+var svg = document.querySelector('object')
+console.log('%c⧭', 'color: #e50000', svg);
 
-navLinks.forEach((b) => {
-  b.onclick = () => {
-    let destinationElement = document.getElementById(b.dataset.destination);
-    let distanceToDestination = destinationElement.offsetTop - window.pageYOffset - 62;
-    if (overlayVisible) {
-      navOverlay.style.width = "0%";
-      overlayVisible = false;
-    }
-    if (typeof window.scrollBy === 'function' && window.scrollBy) {
-      window.scrollBy({top: distanceToDestination + 30, left: 0, behavior: "smooth"});
-    } else {
-      window.scrollBy(0, distanceToDestination + 30);
-    }
-  }
-});
+var polygonsData = [];
+var polygons;
 
-window.onscroll = () => {
-  if (window.scrollY > 0) {
-    navWide.classList.add('nav-scroll');
-    navOverlay.firstElementChild.style.margin = "20.5px 29px 25px 31px";
-  } else {
-    navWide.classList.remove('nav-scroll');
-    navOverlay.firstElementChild.style.margin = "31px 30px 25px 30px";
+svg.onload = function() {
+  polygons = svg.contentDocument.querySelectorAll('polygon')
+  for (const polygon of polygons) {
+    const fill = polygon.getAttribute('fill');
+    const coordsPairs = polygon.getAttribute('points').split(' ');
+    const coords = coordsPairs.map(el => el.split(','));
+    polygonsData.push({ coords, fill })
+    // console.log('%c⧭', 'color: #917399', coords);
   }
-};
+  // console.log('%c⧭', 'color: #00a3cc', polygonsData);
+  console.log('%c⧭', 'color: #d90000', polygonsData);
+  var i = 0
+  for (const polygon of polygons) {
+    polygon.setAttribute('points', polygonsData[i + 1].coords.join(' '));
+    i++;
+  }
+}
+
+
+ 
