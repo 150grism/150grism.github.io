10bc1933e9ff0512f6513b55dfc8b8a886803777
Grigory Moshkevich <rehfres@gmail.com>
2019-02-07 17:50:15 +0300

    added mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+console.log(process.env.NODE_ENV)
 
 module.exports = {
   devServer: {
@@ -14,10 +16,15 @@ module.exports = {
     rules: [{
       test: /\.scss$/,
       use: [
-        "style-loader",
-        "css-loader",
-        "sass-loader"
+        process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+        'css-loader',
+        'sass-loader'
       ]
     }]
-  }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    })
+  ]
 };
